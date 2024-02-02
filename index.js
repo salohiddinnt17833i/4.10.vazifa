6e1b06fbb1c006fbee0e6a8e3b49e64bb08424d1
salohiddinnt17833i <salohiddindevoleper@gmail.com>
2024-02-02 13:08:26 +0500

    bajarildi

---
+++ index.js
@@ -0,0 +1,45 @@
+//1-masala
+// let arr = [1, 12, 2, 22, 32, 21, 32]
+
+// function masala1(arr) {
+//     let result = arr.filter(num => num > 10);
+//     return result
+// }
+// console.log(masala1(arr));
+
+
+//2-masala
+// let arr = [1, 2, 3, 4, 5, 6, 7]
+// function kubi(arr) {
+//     let res = arr.map(num => num ** 3)
+//     return res
+// }
+// console.log(kubi(arr));
+
+
+//3-masala
+// let arr = [30, 25, 45, 33];
+
+// function karrali(arr) {
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 3 == 0 && arr[i] % 5 == 0) {
+//             return true;
+//         } else
+//             return false
+//     }
+
+// }
+// console.log(karrali(arr));
+
+
+
+
+
+//5-masala
+// let mas = [1, 2, 3, 4, 5]
+// let arr = mas.reverse().map((v) => {
+//     return v.toString()
+// })
+
+// console.log(arr);
